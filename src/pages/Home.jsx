ea83689951b0cca15b3fa2aa0a07f1fbf5c14936
mintmintra <mintracss019@gmail.com>
2022-11-26 09:44:10 +0700

    Timer count

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,7 @@ import heroImg from '../assets/images/hero-img.png'
 import '../styles/home.css'
 import Services from '../services/Services'
 import ProductsList from '../components/UI/ProductsList'
+import Clock from '../components/UI/Clock'
 import products from '../assets/data/products'
 import counterImg from '../assets/images/counter-timer-img.png'
 
@@ -77,8 +78,18 @@ const Home = () => {
     <section className="timer__count">
       <Container>
         <Row>
-          <Col lg='6' md='6'></Col>
           <Col lg='6' md='6'>
+            <div className="clock__top-content">
+              <h4 className="text-white fs-6 mb-2">Limited Offers</h4>
+              <h3 className="text-white fs-5 mb-3">Quality Armchair</h3>
+            </div>
+            <Clock />
+
+            <motion.button whileTap={{ scale: 1.2 }} className="buy__btn store__btn">
+              <Link to="/shop">Visit Store</Link>
+            </motion.button>
+          </Col>
+          <Col lg='6' md='6' className="text-end">
             <img src={counterImg} alt="" />
           </Col>
         </Row>
