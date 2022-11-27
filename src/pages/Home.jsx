9cd2bbffa7d903397784dbbcaea2800105145fd1
mintmintra <mintracss019@gmail.com>
2022-11-27 12:28:02 +0700

    New Arrivals

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,6 +15,8 @@ const Home = () => {
 
   const [trendingProducts, setTrendingProducts] = useState([])
   const [bestSalesProducts, setBestSalesProducts] = useState([])
+  const [mobileProducts, setMobileProducts] = useState([])
+  const [wirelessProducts, setWirelessProducts] = useState([])
   const year = new Date().getFullYear()
 
   useEffect(()=>{
@@ -26,8 +28,18 @@ const Home = () => {
       (item) => item.category === "sofa"
     );
 
+    const filteredMobileProducts = products.filter(
+      (item) => item.category === "mobile"
+    );
+
+    const filteredWirelessProducts = products.filter(
+      (item) => item.category === "wireless"
+    );
+
     setTrendingProducts(filteredTrendingProducts)
     setBestSalesProducts(filteredBestSalesProducts)
+    setMobileProducts(filteredMobileProducts)
+    setWirelessProducts(filteredWirelessProducts)
   }, [])
   return <Helmet title={'Home'}>
     <section className="hero__section">
@@ -92,6 +104,18 @@ const Home = () => {
           <Col lg='6' md='6' className="text-end">
             <img src={counterImg} alt="" />
           </Col>
+        </Row>
+      </Container>
+    </section>
+
+    <section className="new__arrivals">
+      <Container>
+        <Row>
+          <Col lg="12" className="text-center">
+            <h2 className="section__title">New Arrivals</h2>
+          </Col>
+          <ProductsList data={mobileProducts} />
+          <ProductsList data={wirelessProducts} />
         </Row>
       </Container>
     </section>
