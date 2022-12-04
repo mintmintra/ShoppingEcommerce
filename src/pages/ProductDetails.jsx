a2c140e65ee27696e7527daccaab6259f319768d
mintmintra <mintracss019@gmail.com>
2022-12-05 01:19:38 +0700

    product detail

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,9 +1,51 @@
 import React from 'react'
+import { Container, Row, Col } from "reactstrap"
+import { useParams } from 'react-router-dom'
+import products from '../assets/data/products'
+import Helmet from '../components/Helmet/Helmet'
+import CommonSection from '../components/UI/CommonSection'
+import "../styles/product-details.css"
+import { motion } from 'framer-motion'
 
 const ProductDetails = () => {
-  return (
-    <div>ProductDetails</div>
-  )
+  const { id } = useParams();
+  const product = products.find((item) => item.id === id);
+  const { imgUrl, productName, price, avgRating, review, description, shortDesc } = product;
+
+  return <Helmet>
+    <CommonSection />
+
+    <section className='pt-0'>
+      <Container>
+        <Row>
+          <Col lg="6">
+            <img src={imgUrl} alt="" />
+          </Col>
+          <Col lg="6">
+            <div className="product__details">
+              <h2>{productName}</h2>
+              <div className="product__rating d-flex align-items-center gap-5 mb-3">
+                <div>
+                  <span><i class="ri-star-s-fill"></i></span>
+                  <span><i class="ri-star-s-fill"></i></span>
+                  <span><i class="ri-star-s-fill"></i></span>
+                  <span><i class="ri-star-s-fill"></i></span>
+                  <span><i class="ri-star-half-s-line"></i></span>
+                </div>
+
+                <p>
+                  (<span>{avgRating}</span> ratings)
+                </p>
+              </div>
+              <span className='product__price'>${price}</span>
+              <p className='mt-3'>{shortDesc}</p>
+              <motion.button whileTap={{ scale: 1.2}} className='buy__btn'>Add to Cart</motion.button>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  </Helmet>
 }
 
 export default ProductDetails
