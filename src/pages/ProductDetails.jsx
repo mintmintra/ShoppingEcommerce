61f3f822c468fca7d493eaabcd25f4bf444fe0c8
mintmintra <mintracss019@gmail.com>
2022-12-05 22:46:33 +0700

    product detail

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { Container, Row, Col } from "reactstrap"
 import { useParams } from 'react-router-dom'
 import products from '../assets/data/products'
@@ -8,12 +8,14 @@ import "../styles/product-details.css"
 import { motion } from 'framer-motion'
 
 const ProductDetails = () => {
+
+  const [tab,setTab] = useState('desc')
   const { id } = useParams();
   const product = products.find((item) => item.id === id);
-  const { imgUrl, productName, price, avgRating, review, description, shortDesc } = product;
+  const { imgUrl, productName, price, avgRating, reviews, description, shortDesc } = product;
 
-  return <Helmet>
-    <CommonSection />
+  return <Helmet title={productName}>
+    <CommonSection title={productName} />
 
     <section className='pt-0'>
       <Container>
@@ -41,6 +43,39 @@ const ProductDetails = () => {
               <p className='mt-3'>{shortDesc}</p>
               <motion.button whileTap={{ scale: 1.2}} className='buy__btn'>Add to Cart</motion.button>
             </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+
+    <section>
+      <Container>
+        <Row>
+          <Col lg="12">
+            <div className="tab__wrapper d-flex align-items-center gap-5">
+              <h6 className={`${tab === "desc" ? "active__tab" : ""}`} onClick={()=> setTab('desc')}>Description</h6>
+              <h6 className={`${tab === "rev" ? "active__tab" : ""}`} onClick={()=> setTab('rev')}>Reviews ({reviews.length})</h6>
+            </div>
+
+            {
+              tab ==='desc' ? <div className="tab__content mt-5">
+              <p>{description}</p>
+            </div> : (
+              <div className="product__review mt-5">
+                <div className="review__wrapper">
+                  <ul>
+                    {
+                      reviews?.map((item,index)=>(
+                        <li><span>{item.rating} ( rating)</span>
+                        <p>{item.text}</p>
+                        </li>
+                      ))
+                    }
+                  </ul>
+                </div>
+              </div>
+            )
+            }
           </Col>
         </Row>
       </Container>
