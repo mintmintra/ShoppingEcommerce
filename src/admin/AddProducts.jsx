96d5ab8b4f371d6cb2bbac1630df0034fed410e8
mintmintra <mintracss019@gmail.com>
2022-12-19 20:58:59 +0700

    addproduct

--- src/admin/AddProducts.jsx
+++ src/admin/AddProducts.jsx
@@ -1,5 +1,6 @@
 import React,{useState} from 'react'
 import { Container, Row, Col, Form, FormGroup } from 'reactstrap';
+import { toast } from 'react-toastify';
 
 const AddProducts = () => {
 
@@ -9,31 +10,46 @@ const AddProducts = () => {
   const [enterCategory, setEnterCategory] = useState('')
   const [enterPrice, setEnterPrice] = useState('')
   const [enterProductImg, setEnterProductImg] = useState(null)
+  const [loading, setLoading] = useState(false)
+
+  const addProduct = async(e) => {
+    e.preventDefault()
+    const product = {
+      title: enterTitle,
+      shortDesc: enterShortDesc,
+      description: enterDescription,
+      category: enterCategory,
+      price: enterPrice,
+      imgUrl: enterProductImg
+    };
+    toast.success('product successfully added!')
+    console.log(product)
+  }
 
   return <section>
     <Container>
       <Row>
         <Col lg="12">
           <h4 className="mb-5">Add Product</h4>
-          <Form>
+          <Form onSubmit={addProduct}>
             <FormGroup className="form__group">
               <span>Product title</span>
-              <input type="text" placeholder="Double sofa" value={enterTitle} onChange={e=> setEnterTitle(e.target.value)} />
+              <input type="text" placeholder="Double sofa" value={enterTitle} onChange={e=> setEnterTitle(e.target.value)} required />
             </FormGroup>
             <FormGroup className="form__group">
               <span>Short Description</span>
-              <input type="text" placeholder="lorem......" value={enterShortDesc} onChange={e=> setEnterShortDesc(e.target.value)} />
+              <input type="text" placeholder="lorem......" value={enterShortDesc} onChange={e=> setEnterShortDesc(e.target.value)} required />
             </FormGroup>
 
             <FormGroup className="form__group">
               <span>Description</span>
-              <input type="text" placeholder="Description...." value={enterDescription} onChange={e=> setEnterDescription(e.target.value)} />
+              <input type="text" placeholder="Description...." value={enterDescription} onChange={e=> setEnterDescription(e.target.value)} required />
             </FormGroup>
 
             <div className="d-flex align-items-center justify-content-between gap-5">
               <FormGroup className="form__group w-50">
                 <span>Price</span>
-                <input type="number" placeholder="$100" value={enterPrice} onChange={e=> setEnterPrice(e.target.value)} />
+                <input type="number" placeholder="$100" value={enterPrice} onChange={e=> setEnterPrice(e.target.value)} required />
               </FormGroup>
 
               <FormGroup className="form__group w-50">
@@ -51,7 +67,7 @@ const AddProducts = () => {
             <div>
             <FormGroup className="form__group">
                 <span>Product Image</span>
-                <input type="file" />
+                <input type="file" onChange={e => setEnterProductImg(e.target.files[0])} required />
               </FormGroup>
             </div>
             <button className="buy__btn" type="submit">Add Product</button>
